8353a2ee375508a217b6c8f62232689a79d1393e
dionlan <dionlan.alves@gmail.com>
2022-12-26 09:58:29 -0300

    Commit inicial

---
+++ src/main/java/com/breakfast/breakfastapi/BreakfastApiApplication.java
@@ -0,0 +1,13 @@
+package com.breakfast.breakfastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakfastApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breakfast/breakfastapi/BreakfastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakfast.breakfastapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
